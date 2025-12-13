cb8d9bfc430a2c0b47f2928dddab5624b533308b
akshay-chenna <akshaychenna@gmail.com>
2025-12-13 12:29:53 +0000

    feat: Add GPC3 RF3 model analysis notebooks and update run scripts, and modify HLA0201 interface finding scripts and data.

--- hla0201_p53r175h/run_2/find_interface.py
+++ hla0201_p53r175h/run_2/find_interface.py
@@ -5,8 +5,8 @@ import sys
 f = sys.argv[1]
 
 u = mda.Universe(f,f)
-y = np.array([73,76,77,80,81,84,143,146,147,150,152,187,188,189])
-z = np.array([187,188,189])
+y = np.array([73,76,77,80,81,84,143,146,147,150,152,237,238,239])
+z = np.array([237,238,239])
 x = u.select_atoms('(chainID A) and (around 4.5 chainID B)')
 x = np.unique(x.resids)
 fraction = np.mean(np.isin(y,x))
---
+++ hla0201_p53r175h/run_5/volume_data/commands.sh
@@ -0,0 +1,1 @@
+for i in Accepted/*pdb ; do python find_interface.py $i >> hotspot_overlap.txt ; done
